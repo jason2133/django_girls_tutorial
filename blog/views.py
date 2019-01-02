02b6b27935a5460bf3f5bb418a95ae489f7779a7
Jason <jason2133@likelion.org>
2019-01-03 00:08:41 +0900

    Django 템플릿

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,26 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+# from django.utils import timezone
+# from .models import Post
 
-# Create your views here.
+# # from . (from 다음에 있는 마침표 .)은 현재 디렉토리, 애플리케이션을 의미함!
 
-# Django 뷰 만들기!
+# # Create your views here.
 
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+# # Django 뷰 만들기!
+
+# def post_list(request):
+#     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+#     return render(request, 'blog/post_list.html', {})
 
 # C:/Users/jason/Desktop/myweb2/myweb3/blog/templates/blog/post_list.html
+
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+    
 
 
